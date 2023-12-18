552899bebe42a45321fc13dffbabcbef224fbfb4
Chernii-Gospodin <153846844+Chernii-Gospodin@users.noreply.github.com>
2023-12-18 16:56:53 +0500

    Update backend_dialogs.py

--- backend_dialogs.py
+++ backend_dialogs.py
@@ -5,7 +5,60 @@ from yandex.cloud.lockbox.v1.payload_service_pb2 import GetPayloadRequest
 from yandex.cloud.lockbox.v1.payload_service_pb2_grpc import PayloadServiceStub
 
 boto_session = None
+BUCKET_NAME = 'opengeimer-cloud'
+BUTTONS_ENABLED = {True: [
+                    {
+                        "title": "Создать макрос",
+                        "payload": {
+                                    "original_utterance": "Создать макрос",
+                                },
+                        "hide": 'true'
+                    },
+                    {
+                        "title": "Использовать макрос",
+                        "payload": {
+                                    "original_utterance": "Использовать макрос",
+                                },
+                        "hide": 'true'
+                    },
+                    {
+                        "title": "Изменить макрос",
+                        "payload": {
+                                    "original_utterance": "Изменить макрос",
+                                },
+                        "hide": 'true'
+                    },
+                    {
+                        "title": "Удалить макрос",
+                        "payload": {
+                                    "original_utterance": "Удалить макрос",
+                                },
+                        "hide": 'true'
+                    },
+                    {
+                        "title": "Просмотреть макросы",
+                        "payload": {
+                                    "original_utterance": "Просмотреть макросы",
+                                },
+                        "hide": 'true'
+                    },
+                ],
+                False: []}
 
+def check_user_message(func):
+    global BUCKET_NAME
+    def wrapper(original_utterance, USER_ID, s3, session_state):
+        macros_name = original_utterance.lower()
+        macros = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID+'/'+macros_name+'.txt')
+        if 'Contents' in macros:
+            text, session_state['user_action'], session_state['user_links'], buttons_status = func(macros_name, USER_ID, s3, session_state)
+        else:
+            text = 'Такого макроса не существует'
+            session_state['user_action'] = ''
+            session_state['user_links'] = 'false'
+            buttons_status = True
+        return text, session_state['user_action'], session_state['user_links'], buttons_status
+    return wrapper
 
 def get_boto_session():
     global boto_session
@@ -36,191 +89,167 @@ def get_boto_session():
     )
     return boto_session
 
+def create_macros(original_utterance, USER_ID, s3, session_state, user_links):
+    global BUCKET_NAME
+    if user_links == 'false':
+        text = 'Введите все ссылки, абсолютные пути нужных вам файлов или команды к операционной системе в формате, например:\nСсылка\n"Путь"\nСсылка\nСсылка\nКоманда'
+        macros_name = original_utterance.lower()
+        s3.put_object(Bucket=BUCKET_NAME,
+                        Key=f"{USER_ID}/{macros_name}.txt",
+                        Body='',
+                        StorageClass='STANDARD',
+                        ContentEncoding='UTF-8')
+        session_state['user_action'] = 'Create'
+        session_state['user_links'] = 'true'
+        buttons_status = False
+    elif user_links == 'true':
+        user_macroses = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
+        for macros in user_macroses['Contents']:
+            if macros['Size'] == 0:
+                macros_path = macros['Key']
+        s3.put_object(Bucket=BUCKET_NAME,
+                        Key=macros_path,
+                        Body=original_utterance,
+                        StorageClass='STANDARD',
+                        ContentEncoding='UTF-8')
+        text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
+        session_state['user_action'] = ''
+        session_state['user_links'] = 'false'
+        buttons_status = True
+    return text, session_state['user_action'], session_state['user_links'], buttons_status
 
-def pressed_button(BUCKET_NAME, USER_ID, original_utterance, folder):
+@check_user_message
+def change_macros_links_false(macros_name, USER_ID, s3, session_state):
+    global BUCKET_NAME
+    macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID+'/'+macros_name+'.txt')
+    macros_content = macros['Body'].read().decode('utf-8')
+    s3.put_object(Bucket=BUCKET_NAME, Key=USER_ID+'/'+macros_name+'.txt', Body='')
+    text = 'Введите все ссылки, абсолютные пути нужных вам файлов или команды к операционной системе в формате, например:\nСсылка\n"Путь"\nСсылка\nСсылка\nКоманда\n\nВот как этот макрос выглядит сейчас:\n' + macros_content
+    session_state['user_action'] = 'Change'
+    session_state['user_links'] = 'true'
+    buttons_status = False
+    return text, session_state['user_action'], session_state['user_links'], buttons_status
+
+def change_macros_links_true(original_utterance, USER_ID, s3):
+    global BUCKET_NAME
+    user_macroses = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
+    for macros in user_macroses['Contents']:
+        if macros['Size'] == 0:
+            macros_path = macros['Key']
+    s3.put_object(Bucket=BUCKET_NAME,
+                    Key=macros_path,
+                    Body=original_utterance,
+                    StorageClass='STANDARD',
+                    ContentEncoding='UTF-8')
+    text = "Макрос изменён\nЧто дальше? Если забыли свой USER_ID, введите любое сообщение"
+    return text
+
+@check_user_message
+def use_macros(macros_name, USER_ID, s3, session_state):
+    global BUCKET_NAME
+    macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID+'/'+macros_name+'.txt')
+    macros_content = str(macros['Body'].read().decode('utf-8'))
+    s3.put_object(Bucket=BUCKET_NAME,
+                    Key=USER_ID+'/'+macros_name+'.txt',
+                    Body=macros_content + '\nFLAG',
+                    StorageClass='STANDARD',
+                    ContentEncoding='UTF-8')
+    text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
+    session_state['user_action'] = ''
+    session_state['user_links'] = 'false'
+    buttons_status = True
+    return text, session_state['user_action'], session_state['user_links'], buttons_status
+
+def pressed_button(original_utterance, user_folder):
     utterances_and_contents = {'Создать макрос': [
-        'Create',
-        'Придумайте название для макроса\nВАЖНО! Если хотите вызывать в будущем макрос голосом, дайте ему название на русском'
-    ],
-        'Использовать макрос': [
-            'Use',
-            'Запустите программу перед использованием макроса, после можете ввести нужное название'
-        ],
-        'Изменить макрос': [
-            'Change',
-            'Введите название макроса, который хотите изменить:'
-        ],
-        'Удалить макрос': [
-            'Delete',
-            'Введите название макроса, который хотите удалить:'
-        ],
-        'Просмотреть макросы': [
-            'View',
-            'Введите название макроса, который хотите посмотреть'
-        ]
-    }
+                                    'Create',
+                                    'Придумайте название для макроса\nВАЖНО! Если хотите вызывать в будущем макрос голосом, дайте ему название на русском'
+                                                ],
+                              'Использовать макрос': [
+                                    'Use',
+                                    'Запустите программу перед использованием макроса, после можете ввести нужное название'
+                                                    ],
+                              'Изменить макрос': [
+                                    'Change',
+                                    'Введите название макроса, который хотите изменить:'
+                                                ],
+                              'Удалить макрос': [
+                                    'Delete',
+                                    'Введите название макроса, который хотите удалить:'
+                                                ],
+                              'Просмотреть макросы': [
+                                    'View',
+                                    'Введите название макроса, который хотите посмотреть'
+                                                    ]
+                                }
     text = ''
     user_action = ''
-    buttons_enabled = True
-    if 'Contents' in folder:
-        folder_content = folder['Contents']
-        for key in range(len(folder_content)):
-            text += str(str(key + 1) + '. ' + folder_content[key]['Key'].split('/')[-1][:-4]) + '\n'
+    buttons_status = True
+    if 'Contents' in user_folder:
+        user_folder_content = user_folder['Contents']
+        for key in range(len(user_folder_content)):
+            text += str(str(key+1) + '. ' + user_folder_content[key]['Key'].split('/')[-1][:-4]) + '\n'
     elif original_utterance == 'Создать макрос':
         text = text
     else:
         text = 'У вас нет макросов'
-        return text, user_action, buttons_enabled
+        return text, user_action, buttons_status
     for key in utterances_and_contents.keys():
         if original_utterance == key:
             user_action = utterances_and_contents[key][0]
             text += utterances_and_contents[key][1]
-            buttons_enabled = False
-    return text, user_action, buttons_enabled
-
+            buttons_status = False
+    return text, user_action, buttons_status
 
 async def handler(event, context):
+    global BUCKET_NAME, BUTTONS_ENABLED
     # Entry-point for Serverless Function.
     # :param event: request payload.
     # :param context: information about current execution context.
     # :return: response to be serialized as JSON.
     session = get_boto_session()
-    BUCKET_NAME = 'opengeimer-cloud'
     USER_ID = event["session"]["user"]["user_id"]
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net'
     )
     text = "Привет, пользователь.\n\nЧтобы данный навык работал исправно, скачай отсюда приложение: https://clck.ru/378m8o или https://github.com/Chernii-Gospodin/AliceMacros.\
-\n\nВот твой USER_ID, необходимый для приложения: " + USER_ID
+\n\nВот твой USER_ID, необходимый для приложения: " + USER_ID + "\nПодготовте нужные ссылки и пути заранее, так как Алиса сбрасывается, если нажать в браузере на что-то кроме неё"
     session_state = {'user_action': '', 'user_links': 'false'}
     macros_name = ''
     buttons_status = True
-    buttons_enabled = {True: [
-        {
-            "title": "Создать макрос",
-            "payload": {
-                "original_utterance": "Создать макрос",
-            },
-            "hide": 'true'
-        },
-        {
-            "title": "Использовать макрос",
-            "payload": {
-                "original_utterance": "Использовать макрос",
-            },
-            "hide": 'true'
-        },
-        {
-            "title": "Изменить макрос",
-            "payload": {
-                "original_utterance": "Изменить макрос",
-            },
-            "hide": 'true'
-        },
-        {
-            "title": "Удалить макрос",
-            "payload": {
-                "original_utterance": "Удалить макрос",
-            },
-            "hide": 'true'
-        },
-        {
-            "title": "Просмотреть макросы",
-            "payload": {
-                "original_utterance": "Просмотреть макросы",
-            },
-            "hide": 'true'
-        },
-    ],
-        False: []}
 
     if event['request']['type'] == 'ButtonPressed':
-        folder = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
-        text, session_state["user_action"], buttons_status = pressed_button(BUCKET_NAME, USER_ID,
-                                                                            event['request']['payload'][
-                                                                                'original_utterance'], folder)
+        user_folder = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
+        text, session_state["user_action"], buttons_status = pressed_button(event['request']['payload']['original_utterance'], user_folder)
 
     if 'user_action' in event['state']['session']:
 
         if event['state']['session']['user_action'] == 'Create':
-            if event['state']['session']['user_links'] == 'false':
-                text = 'Введите все ссылки, абсолютные пути нужных вам файлов или команды к операционной системе в формате, например:\nСсылка\n"Путь"\nСсылка\nСсылка\nКоманда'
-                macros_name = event['request']['original_utterance'].lower()
-                s3.put_object(Bucket=BUCKET_NAME,
-                              Key=f"{event['session']['user']['user_id']}/{macros_name}.txt",
-                              Body='',
-                              StorageClass='STANDARD',
-                              ContentEncoding='UTF-8')
-                session_state['user_action'] = 'Create'
-                session_state['user_links'] = 'true'
-                buttons_status = False
-            elif event['state']['session']['user_links'] == 'true':
-                user_macroses = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
-                for macros in user_macroses['Contents']:
-                    if macros['Size'] == 0:
-                        macros_name = macros['Key']
-                s3.put_object(Bucket=BUCKET_NAME,
-                              Key=macros_name,
-                              Body=event['request']['original_utterance'],
-                              StorageClass='STANDARD',
-                              ContentEncoding='UTF-8')
-                text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
-
+            text, session_state['user_action'], session_state['user_links'], buttons_status = create_macros(event['request']['original_utterance'], USER_ID, s3, session_state, event['state']['session']['user_links'])
+        
         if event['state']['session']['user_action'] == 'Use':
-            macros_name = event['request']['original_utterance'].lower()
-            macros = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID + '/' + macros_name + '.txt')
-            if 'Contents' in macros:
-                macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID + '/' + macros_name + '.txt')
-                macros_content = str(macros['Body'].read().decode('utf-8'))
-                s3.put_object(Bucket=BUCKET_NAME,
-                              Key=USER_ID + '/' + macros_name + '.txt',
-                              Body=macros_content + '\nFLAG',
-                              StorageClass='STANDARD',
-                              ContentEncoding='UTF-8')
-                text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
-            else:
-                text = 'Такого макроса не существует'
+            text, session_state['user_action'], session_state['user_links'], buttons_status = use_macros(event['request']['original_utterance'], USER_ID, s3, session_state)
 
         if event['state']['session']['user_action'] == 'Change':
             if event['state']['session']['user_links'] == 'false':
-                macros_name = event['request']['original_utterance'].lower()
-                macros = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID + '/' + macros_name + '.txt')
-                if 'Contents' in macros:
-                    macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID + '/' + macros_name + '.txt')
-                    macros_content = macros['Body'].read().decode('utf-8')
-                    s3.put_object(Bucket=BUCKET_NAME, Key=USER_ID + '/' + macros_name + '.txt', Body='')
-                    text = 'Введите все ссылки, абсолютные пути нужных вам файлов или команды к операционной системе в формате, например:\nСсылка\n"Путь"\nСсылка\nСсылка\nКоманда\n\nВот как этот макрос выглядит сейчас:\n' + macros_content
-                    session_state['user_action'] = 'Change'
-                    session_state['user_links'] = 'true'
-                    buttons_status = False
-                else:
-                    text = 'Такого макроса не существует'
+                text, session_state['user_action'], session_state['user_links'], buttons_status = change_macros_links_false(event['request']['original_utterance'], USER_ID, s3, session_state)
             elif event['state']['session']['user_links'] == 'true':
-                user_macroses = s3.list_objects(Bucket=BUCKET_NAME, Prefix=USER_ID)
-                for macros in user_macroses['Contents']:
-                    if macros['Size'] == 0:
-                        macros_name = macros['Key']
-                s3.put_object(Bucket=BUCKET_NAME,
-                              Key=macros_name,
-                              Body=event['request']['original_utterance'],
-                              StorageClass='STANDARD',
-                              ContentEncoding='UTF-8')
-                text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
+                text = change_macros_links_true(event['request']['original_utterance'], USER_ID, s3)
 
         if event['state']['session']['user_action'] == 'Delete':
             macros_name = event['request']['original_utterance'].lower()
-            deleted_object = [{'Key': USER_ID + '/' + macros_name + '.txt'}]
+            deleted_object = [{'Key': USER_ID+'/'+macros_name+'.txt'}]
             s3.delete_objects(Bucket=BUCKET_NAME, Delete={'Objects': deleted_object})
-            text = "Что дальше? Если забыли свой USER_ID, введите любое сообщение"
+            text = "Макрос успешно удалён\nЧто дальше? Если забыли свой USER_ID, введите любое сообщение"
 
         if event['state']['session']['user_action'] == 'View':
             macros_name = event['request']['original_utterance'].lower()
-            macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID + '/' + macros_name + '.txt')
+            macros = s3.get_object(Bucket=BUCKET_NAME, Key=USER_ID+'/'+macros_name+'.txt')
             macros_content = macros['Body'].read().decode('utf-8')
-            text = 'Вот как этот макрос выглядит сейчас:\n' + macros_content
+            text = 'Вот как этот макрос выглядит сейчас:\n' + macros_content 
 
-    buttons = buttons_enabled[buttons_status]
+    buttons = BUTTONS_ENABLED[buttons_status]
 
     return {
         'version': event['version'],
